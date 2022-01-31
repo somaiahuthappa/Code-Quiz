40e3e74a3b6d951edbd2061db897558157b346fc
Som's Laptop <somaiahuthappa@gmail.com>
2022-01-31 10:52:52 -0500

    added-basic-HTML-and-CSS

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+var questions = [
+    {
+        question: 'How to align a container to the top of its container in CSS?',
+        answers: [
+            { text: 'text-align:top', answer: false },
+            { text: "margin-align:top", answer: false },
+            { text: "overflow-top", answer: false },
+            { text: "vertical-align: top", answer: true }
+        ]
+    },
+        
+        
+        {
+        question: 'Which of these is not a type of loop',
+        answers: [
+            { text: 'while', answer: false },
+            { text: 'if', answer: true },
+            { text: 'for', answer: false },
+            { text: 'continue', answer: false }
+        ]
+    },
+        {
+        question: 'What does JSON.parse() do',
+        answers: [
+            { text: 'Retrieves an item from local storage', answer: false },
+            { text: 'Turns a string into an object', answer: false },
+            { text: 'Combines two strings', answer: false },
+            { text: 'Analyses items in local storage', answer: false }
+        ]
+    },
+    {
+        question: 'Inside which HTML element do you place the Javascript?',
+        answers: [
+            { text: '<javascript>', answer: false },
+            { text: '<js>', answer: false },
+            { text: '<scripting>', answer: false },
+            { text: '<script>', answer: true }
+        ]
+    },
+    {
+        question: 'How do you round the number 7.25 to the nearest integer?',
+        answers: [
+            { text: 'Math.rnd(7.25)', answer: false },
+            { text: 'Math.round(7.25)', answer: true },
+            { text: 'rnd(7.25)', answer: false },
+            { text: 'round(7.25)', answer: false }
+        ]
+
+    }
+
+]
